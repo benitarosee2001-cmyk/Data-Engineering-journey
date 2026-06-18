e399bf21c9101c558f76bd445b724009d8f0be42
Benita <benitarosee2001@gmail.com>
2026-06-18 23:59:50 +0330

    add: variables and operators practice

--- python_exercises/day01.py
+++ python_exercises/day01.py
@@ -1,5 +1,30 @@
+# Exercise 1 variable
 name = "benita"
 age = 24
 city = "Tehran"
 
 print(f" My name is {name} and I'm {age} I live in {city}")
+
+
+#Exercise 3 Operators
+a = 17
+b = 5
+print(a // b)
+print(a % b)
+print(a ** b)
+
+
+price = 1500000
+discount = price * 20 / 100
+final = price - discount
+print(final)
+
+
+#Exercise 4
+number = 365
+units = number % 10
+tens = number // 10 % 10
+hundreds = number // 100
+print(units)
+print(tens)
+print(hundreds)
