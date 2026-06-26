7a5b1e0d317c80f12cc20acc964dd43326b0d903
Benita <benitarosee2001@gmail.com>
2026-06-26 13:47:00 +0330

    day07 loops

---
+++ python_exercises/day07.py
@@ -0,0 +1,31 @@
+#Exercise 1
+for i in range(1, 5):
+    for j in range(1, 5):
+        print("*", end = " ")
+    print()
+
+
+#Exercise 2
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(i, j, end = " ")
+    print()
+
+
+#Exercise 3
+for i in range(1, 5):
+    for j in range(1, 5):
+        print(j, end = " ")
+    print()
+
+
+#Exercise 4
+for i in range(A, B, C):
+    for i in range(A, B, C):
+
+
+
+#Exercise 5
+for i in range(1, 6):
+    for j in range(1, 6):
+        
