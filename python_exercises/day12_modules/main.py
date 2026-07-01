402f918863508177bad67159d455052f290e5aa0
Benita <benitarosee2001@gmail.com>
2026-07-01 19:14:42 +0330

    Day 12: Python modules

--- python_exercises/day12_modules/main.py
+++ python_exercises/day12_modules/main.py
@@ -1,3 +1,13 @@
 from calc import add
+from string_utils import upper_text as u
+import make_lower
+import square_area
+import person as p
+from person import perso
 
 print(add(3, 5))
+print(u("stray kids"))
+print(make_lower.lower_text("EBHYPEN"))
+print(square_area.func(12, 3))
+print(p.pers("Benita"))
+print(perso(23))
---
+++ python_exercises/day12_modules/person.py
@@ -0,0 +1,5 @@
+def pers(name):
+    return name
+
+def perso(age):
+    return age
