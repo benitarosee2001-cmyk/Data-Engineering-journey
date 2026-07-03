44bbf485b9dfd6f4d5e84e8fc2266f733b21a65c
Benita <benitarosee2001@gmail.com>
2026-07-03 12:15:41 +0330

    Day 14: Python files

--- python_exercises/day13_files/main.py
+++ python_exercises/day13_files/main.py
@@ -1,5 +1,36 @@
-f = open("note90.txt", "x+")
+with open("notes.txt", "w", encoding="utf8") as file:
+    file.write("Hello Python")
 
-print(f.readline(), end=" ")
-print(f.readline(), end=" ")
-print(f.write("jk"))
+
+
+with open("notes.txt", "r", encoding="utf8") as file:
+    print(file.read())
+
+
+
+with open("notes.txt", "a", encoding="utf8") as file:
+    file.write("\nData Engineer")
+
+
+with open("notes.txt", "r", encoding="utf8") as file:
+    print(file.read())
+
+
+
+with open("skills.txt", "x", encoding="utf8") as file:
+    file.write("Python\nSQL\nSpark")
+
+
+
+with open("skills.txt", "r", encoding="utf8") as file:
+    print(file.read())
+
+
+
+with open("profile.txt", "w", encoding="utf8") as file:
+    file.write("Name: Benita\nJob: Data Engineer\nCountry: Germany")
+
+
+
+with open("profile.txt", "r", encoding="utf8") as file:
+    print(file.read())
