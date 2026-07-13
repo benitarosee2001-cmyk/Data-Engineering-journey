c922c62b44c71b71c1b0d9c429921a153b635539
Benita <benitarosee2001@gmail.com>
2026-07-13 12:43:41 +0330

    Project 5: Quize Game

--- Project 05/Quize Game.py
+++ Project 05/Quize Game.py
@@ -20,5 +20,28 @@ def quiz_game():
 
     print("===== Quize Game =====\n")
     for i,question in enumerate(questions, start = 1):
-        print()
-        score
+        print(f"Question {i}")
+        print(question['question'])
+
+        user_answer = input("Your answer: ")
+
+        if user_answer.lower() == question['answer'].lower():
+            score += 1
+        else:
+            print("Wrong!")
+            print(f"Correct Answer: {question['answer']}\n")
+
+            total_question = len(questions)
+            wrong_answer = total_question - score
+            percentage = (score / total_question) * 100
+
+            print("=" * 50)
+            print("\tResult\t")
+            print("=" * 50)
+            print(f"Correct answer: {score}")
+            print(f"Wrong answer: {wrong_answer}")
+            print(f"Score: {percentage:.0f}")
+            print("*" * 50)
+
+
+quiz_game()
