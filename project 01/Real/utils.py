8cd2e190c833212d7e7b28b89a30988f4e8a0e81
Benita <benitarosee2001@gmail.com>
2026-07-25 13:59:17 +0330

    Project: Student Management.

---
+++ project 01/Real/utils.py
@@ -0,0 +1,20 @@
+student_id = 1000
+
+
+def generate_id():
+
+    global student_id
+
+    student_id += 1
+
+    return student_id
+
+
+def update_last_id(students):
+
+    global student_id
+
+    if not students:
+        return
+    
+    student_id = max(student.id for student in students)
