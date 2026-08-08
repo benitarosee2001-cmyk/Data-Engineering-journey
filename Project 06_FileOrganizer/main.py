775c2cafc285053b7ee689756268262d41a8f028
Benita <benitarosee2001@gmail.com>
2026-08-08 15:49:28 +0330

    Add automatic file organization

--- Project 06_FileOrganizer/main.py
+++ Project 06_FileOrganizer/main.py
@@ -1,10 +1,15 @@
 import os
+import shutil
 
 folder = os.path.join(os.path.dirname(__file__), "TestFolder")
 
 files = os.listdir(folder)
 
 for file in files:
+
+    if not os.path.isfile(os.path.join(folder, file)):
+        continue
+
     name, extension = os.path.splitext(file)
 
     if extension in [".jpg", ".png"]:
@@ -27,5 +32,15 @@ for file in files:
 
     else:
         category = "Others"
+
+    category_folder = os.path.join(folder, category)
+
+    os.makedirs(category_folder, exist_ok=True)
+
+    source =os.path.join(folder, file)
+
+    destination = os.path.join(category_folder, file)
+
+    shutil.move(source, destination)
 
     print(f"{file} -> {category}")
